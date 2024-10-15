b7ba3b83a51d6546e2b877cfb9adf27ccef9fd48
Vaishnavikantha <vaishukantha15@gmail.com>
2024-10-15 15:51:33 +0530

    Art Blog Website Commited

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader--hidden");
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild(loader);
+    });
+});
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
